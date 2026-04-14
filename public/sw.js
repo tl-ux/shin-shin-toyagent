e2c55c8a5a510fac320ca8c68b40555bf975bf73
Tal Levenberg <t0547990799@gmail.com>
2026-04-15 01:33:50 +0300

    add PWA service worker

---
+++ public/sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'toyagent-v1';
+const PRECACHE_URLS = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_URLS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((names) => Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).catch(() => caches.match('/index.html')));
+  }
+});
