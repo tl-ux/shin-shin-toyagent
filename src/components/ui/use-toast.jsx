aac786480a5f75426e0e289b84b7d9e6f8a95e5d
Tal Levenberg <t0547990799@gmail.com>
2026-04-29 01:11:47 +0300

    reduce toast duration

--- src/components/ui/use-toast.jsx
+++ src/components/ui/use-toast.jsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 
 const TOAST_LIMIT = 20;
-const TOAST_REMOVE_DELAY = 3000;
+const TOAST_REMOVE_DELAY = 2000;
 
 const actionTypes = {
   ADD_TOAST: "ADD_TOAST",
